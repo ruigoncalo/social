e55384164bedfbd88ab9f243bdb198240c353e5b
ruigoncalo <ruimiguelgoncalo@gmail.com>
2018-09-06 17:37:52 +0100

    Create data models

---
+++ data/src/main/java/com/ruigoncalo/data/model/PostRaw.kt
@@ -0,0 +1,6 @@
+package com.ruigoncalo.data.model
+
+data class PostRaw(val id: Int,
+                   val userId: Int,
+                   val title: String,
+                   val body: String)
---
+++ data/src/main/java/com/ruigoncalo/data/model/UserRaw.kt
@@ -0,0 +1,23 @@
+package com.ruigoncalo.data.model
+
+data class UserRaw(val id: Int,
+                   val name: String,
+                   val username: String,
+                   val email: String,
+                   val address: AddressRaw,
+                   val phone: String,
+                   val website: String,
+                   val company: CompanyRaw)
+
+data class AddressRaw(val street: String,
+                   val suite: String,
+                   val city: String,
+                   val zipCode: String,
+                   val geo: GeoRaw)
+
+data class GeoRaw(val lat: Double,
+               val lng: Double)
+
+data class CompanyRaw(val name: String,
+                   val catchPhrase: String,
+                   val bs: String)
